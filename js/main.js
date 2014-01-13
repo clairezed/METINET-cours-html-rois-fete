8c0bc41ab383bce2922c9e12ed1b85ab500b88f2
clairezed <clairezuliani@gmail.com>
2014-01-13 13:36:15 +0100

    commit parce que je me fais virer de ma place

--- js/main.js
+++ js/main.js
@@ -1,27 +1,9 @@
    $(document).ready(function() {
 
-      // $(".menu-drop-down").each(function() {
-      //   console.log( $(this).height())
-      //   $.data(this, "realHeight", $(this).height());
-      // }).css({ display:"none"  });
-      
-
-      // $( ".item-btn").on("mouseenter", function(){
-      //      var div = $(".menu-drop-down");
-      //       console.log( div.data("realHeight"))
-      //     // div.toggle(function() {
-      //       div.animate({ height: div.data("realHeight") }, 600);
-      // //     }, function() {
-      // //       div.animate({ height: 0 }, 600);
-      // //     }); 
-      // });
-
-
-    $('.item-btn').mouseenter(function () {
+    if(window.matchMedia("(min-width:768px)").matches){
+      $('.item-btn').mouseenter(function () {
       var color = $(this).css('background-color');
-      console.log(color);
       var type = $(this).children('a').attr('class');
-      console.log(type);  
 
       $('.menu-drop-down').slideUp();
         $("#"+ type+"-menu")
@@ -30,12 +12,16 @@
      }
     );
 
-    $('.menu-drop-down').on('mouseleave', function(){
-      $(this).slideUp();
+    $('.item-part').on('mouseleave', function(){
+      $('.menu-drop-down').slideUp();
     })
 
     $('.close-menu').on('click', function(){
       $('.menu-drop-down').slideUp();
     })
+
+    }
+
+    
 
 })
