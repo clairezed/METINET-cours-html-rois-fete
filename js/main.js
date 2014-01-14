ce4959048a3105a51ac77e33fc12c544817d1076
clairezed <clairezuliani@gmail.com>
2014-01-14 13:25:26 +0100

    class active btn

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
 
        $('.item-btn').on('click', function () {
         if( $(window).width()> 480){ //click event only when desktop or tablette
+          $('.item-btn').removeClass("active");
+          $(this).toggleClass("active");
           var type = $(this).children('a').attr('class');
           
           $('.menu-drop-down').not($("#"+ type+"-menu")).slideUp();
@@ -26,6 +28,7 @@
     });
     
     $('.menu-btn').on('click', function(){
+
         $(this).toggleClass("active");
         $('.mobile-item-nav').slideToggle();
       });
